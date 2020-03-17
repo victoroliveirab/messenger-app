b88f9baab76f296a5eda69b84379e2784ba648d9
Victor Oliveira <vob@cesar.school>
2020-03-17 16:42:48 -0300

    Added bootstrap javascript to public.html. Created dropdown menus on the 'more' icons

--- frontend/src/components/TopBar/topbar.components.jsx
+++ frontend/src/components/TopBar/topbar.components.jsx
@@ -21,13 +21,60 @@ class TopBar extends React.Component {
                         <Avatar className="top-bar__avatar" rounded={true} />
                     </div>
                     <div className="top-bar__other-icons">
-                        <FontAwesomeIcon icon={faCircleNotch} />
-                        <FontAwesomeIcon icon={faCommentAlt} />
-                        <FontAwesomeIcon icon={faEllipsisV} />
+                        <button type="button" className="top-bar__button">
+                            <FontAwesomeIcon icon={faCircleNotch} size="2x" />
+                        </button>
+                        <button type="button" className="top-bar__button">
+                            <FontAwesomeIcon icon={faCommentAlt} size="2x" />
+                        </button>
+
+                        <div className="dropdown">
+                            <button
+                                type="button"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                                className="top-bar__button"
+                            >
+                                <FontAwesomeIcon icon={faEllipsisV} size="2x" />
+                            </button>
+                            <div className="dropdown-menu">
+                                <a className="dropdown-item" href="#">
+                                    Profile
+                                </a>
+                                <a className="dropdown-item" href="#">
+                                    Help
+                                </a>
+                                <a className="dropdown-item" href="#">
+                                    Sign Out
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="top-bar__others">
-                    <FontAwesomeIcon icon={faEllipsisV} />
+                    <div className="dropleft">
+                        <button
+                            type="button"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                            className="top-bar__button"
+                        >
+                            <FontAwesomeIcon icon={faEllipsisV} size="2x" />
+                        </button>
+                        <div className="dropdown-menu">
+                            <a className="dropdown-item" href="#">
+                                Profile
+                            </a>
+                            <a className="dropdown-item" href="#">
+                                Help
+                            </a>
+                            <a className="dropdown-item" href="#">
+                                Sign Out
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
