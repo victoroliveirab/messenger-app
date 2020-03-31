1c05834179e9f31644c3244a4207ea8d84015663
Victor Oliveira <vob@cesar.school>
2020-03-31 00:25:11 -0300

    Refactored signup form to exclude @materia-ui from dependencies and use only bootstrap. Work to do on organizing css classes and files

--- frontend/src/components/SignUpForm/signupform.component.jsx
+++ frontend/src/components/SignUpForm/signupform.component.jsx
@@ -1,28 +1,14 @@
-import React, { Component } from "react";
-import Container from "@material-ui/core/Container";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
-import { withStyles } from "@material-ui/core/styles";
-import DatePicker from "../DatePicker/datepicker.component";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import logo from "./logo.png";
 
 import { dispatchPost } from "../../utils/request";
 
 import "./signupform.style.css";
 
-const styles = theme => ({
-    form: {
-        width: "100%"
-    },
-    submit: {
-        margin: theme.spacing(1, 0, 2)
-    }
-});
-
-class Signup extends Component {
+class Signup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -36,18 +22,15 @@ class Signup extends Component {
 
     handleSubmit = async event => {
         event.preventDefault();
-        const {
-            name,
-            username,
-            email,
-            password,
-            password_confirmation
-        } = this.state;
-        if (password !== password_confirmation) {
+        console.log("submit");
+        console.log(this.state);
+        const { name, username, email, password, passwordConfirm } = this.state;
+        if (password !== passwordConfirm) {
             alert("Passwords dont match");
             return;
         }
         const birthday = this.state.birthday.replace(/\//g, "-");
+        console.log(birthday);
         await dispatchPost("/signup", {
             name,
             username,
@@ -60,111 +43,153 @@ class Signup extends Component {
     handleChange = event => {
         //TODO when leave email or username field, async a get from the server to see if
         // entry already in use!
-        const { name, value } = event.target;
-        this.setState({ [name]: value });
+        const { id, value } = event.target;
+        console.log(id + ": " + value);
+        this.setState({ [id]: value });
     };
 
     handleBirthday = date => this.setState({ birthday: date });
 
     render() {
-        const { classes } = this.props;
         return (
-            <Container maxWidth="xs">
-                <form
-                    className={classes.form}
-                    noValidate
-                    onSubmit={this.handleSubmit}
-                >
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                name="name"
-                                label="Full Name"
-                                required
-                                placeholder="Full Name"
-                                fullWidth
-                                onChange={this.handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                type="email"
-                                name="email"
-                                label="E-mail"
-                                required
-                                placeholder="E-mail"
-                                fullWidth
-                                onChange={this.handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                name="username"
-                                label="Username"
-                                required
-                                placeholder="Username"
-                                fullWidth
-                                className="username"
-                                onChange={this.handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <DatePicker updateBirthday={this.handleBirthday} />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                name="password"
-                                label="Password"
-                                required
-                                placeholder="Password"
-                                fullWidth
-                                type="password"
-                                onChange={this.handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                name="password_confirmation"
-                                label="Confirm Password"
-                                required
-                                placeholder="Confirm Password"
-                                fullWidth
-                                type="password"
-                                onChange={this.handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        value="acceptTerms"
-                                        color="primary"
+            <div className="main-wrapper signup-wrapper">
+                <div className="red-rectangle"></div>
+                <div className="signup-form-container container-fluid">
+                    <div className="form-img-wrapper">
+                        <img src={logo} alt="" className="form-img" />
+                    </div>
+                    <div className="signup-form">
+                        <div className="signup-form__header">
+                            <Link to="/login">
+                                <button
+                                    type="button"
+                                    className="btn signup-form__back"
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faArrowLeft}
+                                        size="2x"
                                     />
-                                }
-                                label="I accept terms and conditions"
-                            />
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container justify="flex-end">
-                            <Grid item>
-                                <Link href="/login" variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </form>
-            </Container>
+                                </button>
+                            </Link>
+                            <h2 className="signup-form__title">Sign up</h2>
+                        </div>
+                        <form noValidate onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label className="form-label" htmlFor="name">
+                                    Name
+                                </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="name"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label
+                                    className="form-label"
+                                    htmlFor="username"
+                                >
+                                    Username
+                                </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="username"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label className="form-label" htmlFor="email">
+                                    Email address
+                                </label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="email"
+                                    onChange={this.handleChange}
+                                    aria-describedby="emailHelp"
+                                />
+                                <small
+                                    id="emailHelp"
+                                    className="form-text text-muted"
+                                >
+                                    We'll never share your email with anyone
+                                    else.
+                                </small>
+                            </div>
+                            <div className="form-group">
+                                <label
+                                    className="form-label"
+                                    htmlFor="birthday"
+                                >
+                                    Birthday
+                                </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="birthday"
+                                    onChange={this.handleChange}
+                                    aria-describedby="birthdayHelp"
+                                />
+                                <small
+                                    id="birthdayHelp"
+                                    className="form-text text-muted"
+                                >
+                                    You have to be over 18 to use this app.
+                                </small>
+                            </div>
+                            <div className="form-group">
+                                <label
+                                    className="form-label"
+                                    htmlFor="password"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="password"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label
+                                    className="form-label"
+                                    htmlFor="passwordConfirm"
+                                >
+                                    Confirm Password
+                                </label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="passwordConfirm"
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="form-group form-check">
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input"
+                                    id="exampleCheck1"
+                                    onChange={this.handleChange}
+                                />
+                                <label
+                                    className="form-check-label"
+                                    htmlFor="exampleCheck1"
+                                >
+                                    Check me out
+                                </label>
+                            </div>
+                            <button type="submit" className="btn form-submit">
+                                Sign up
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
 
-export default withStyles(styles)(Signup);
+export default Signup;
--- frontend/src/pages/oldsignup/signup.page.jsx
+++ frontend/src/pages/oldsignup/signup.page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import SignUpForm from "../../components/SignUpForm/signupform.component";
+import SignUpForm from "../../components/OldSignUpForm/signupform.component";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
-const SignUpPage = ({ loggedIn }) => {
+const signUpPage = ({ loggedIn }) => {
     if (loggedIn || sessionStorage.getItem("pitangAuth")) {
         return <Redirect to="/" />;
     }
@@ -14,4 +14,4 @@ const mapStateToProps = state => ({
     loggedIn: state.user.token
 });
 
-export default connect(mapStateToProps)(SignUpPage);
+export default connect(mapStateToProps)(signUpPage);
